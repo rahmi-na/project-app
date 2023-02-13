0b417b35bca1e8a5f42bd726c78701851d8705d0
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2023-02-13 20:23:41 +0700

    fix

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content text-center">
         <div>
-          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+          <p>Copyright © 2023 - Rahmi Nurazizah</p>
         </div>
       </footer>
     </div>

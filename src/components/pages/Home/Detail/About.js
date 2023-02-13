83fc4e41b74287e7b15a887ad55e416b0c1b47f6
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2023-02-13 19:57:05 +0700

    styled done

--- src/components/pages/Home/Detail/About.js
+++ src/components/pages/Home/Detail/About.js
@@ -6,9 +6,9 @@ function About() {
     <div className="">
       <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center ">
         <div className="font-medium order-last md:order-first flex justify-center ">
-          <div>
-            <h1 className=" text-3xl font-medium">About me</h1>
-            <p className=" text-lg text-justify mt-3">
+          <div className="mt-10">
+            <h1 className=" text-2xl md:text-3xl font-medium">About me</h1>
+            <p className=" text-lg text-justify ">
               Experienced in web developer. Having a commitment to long-term
               goals, being persistent in doing something, and being curious
               about new things. Have a goal of being useful to others and have a
